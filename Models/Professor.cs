bd55965ab5af1bc921e4aa32cd25c8e8bff0694c
Luiz Ricardo de Campos <playsonbr@hotmail.com>
2024-09-26 16:52:18 -0400

    first commit

---
+++ Models/Professor.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HerencaEPolimorfismo.Models
+{
+    public class Professor : Pessoa    
+    {
+        public decimal Salario {get; set;}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+using HerencaEPolimorfismo.Models;
+
+Aluno a1 = new Aluno();
+a1.Nome = "Campos";
+a1.Idade = 25;
+a1.Nota = 10;
+
+a1.Apresentar();
